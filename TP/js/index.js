342397a4630fb85b61c399d6a9594dc1319091ca
brunofruet <brunofruet17@gmail.com>
2021-11-18 19:22:17 -0300

    modificaciones bruno

--- TP/js/index.js
+++ TP/js/index.js
@@ -1,31 +1,33 @@
 
   /*validaciones de formulario*/
   var validado = false;
+  let tarifaFinal;
   function validaciones() {
     // obtenemos todos los input radio de tipo de usuario que esten chequeados
     // si no hay ninguno lanzamos alerta
     tipoUsuario = document.querySelector('input[name="user"]:checked'); 
     if(!tipoUsuario) {
       alert('Seleccione tipo de usuario ');
-      return true
+      return
     } 
   
     //alerta para seleccionar zona, tiene que ser distinta a la que esta seleccionada
     selectZone = document.getElementById('zone');
     if(selectZone.value == 0 ) {
       alert('Seleccione la zona a la que pertenece');
-      return true
+      return
     } 
     
     //alerta para que ingrese cantidad de kWh
     kWh = document.getElementById('kWhValue').value;
     if( kWh <= 0 ) {
       alert('Ingrese una cantidad valida');
-      return true
+      return
     }else{
       
       validado = true;
     }
+    calculadora()
   };
 
   function calculadora() {
@@ -42,8 +44,10 @@
       kWhConsumido = document.getElementById('kWhValue').value;
       const costoServicio = 102;
       tarifaFinal = costoServicio + kWhConsumido * costoZona[zona.value] * (1 + iva);
-      alert(tarifaFinal);
-      let tarifa = document.getElementById('resultado');
-      tarifa.innerText = tarifaFinal;
+
     }
+
+    let tarifa = document.getElementById('resultado');
+    tarifa.innerText = "$" + tarifaFinal;
+
   };
